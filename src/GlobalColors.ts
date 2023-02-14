9d11037da3688a74c7c63d2e470d9574707f5edf
Adeoluwa <odejobipaul10@gmail.com>
2023-02-14 23:21:17 +0100

    Global Colors

---
+++ src/GlobalColors.ts
@@ -0,0 +1,15 @@
+export const GlobalColors = {
+    primary: {
+    lemon: '#D3F36A',
+    warm: '#F7F6F0',
+    blue: '#061D37',
+    neutralGrey: '#E6EBEF'
+    },
+    secondary: {
+        charcoal: '#333333',
+        taupe: '#918E78',
+        pale: '#F8DEAA',
+        ivory: '#D8D5C0',
+        slate: '#DCDED3'
+    }
+}
